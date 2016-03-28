5f8935d920e0216917cdde86dbbdd5c7b4370163
Thomas Poncet <thomas.poncet@ensimag.grenoble-inp.fr>
2016-03-28 21:37:48 +0200

    first simple version of the tool

--- js/mdTocUtils.js
+++ js/mdTocUtils.js
@@ -1,21 +1,60 @@
 angular.module('mdToc').factory('MdTocUtils', function(){
   var mdTocUtils = {};
 
+  function getTitleLevel(titleLine) {
+    if (titleLine !== "" && titleLine.charAt(0) === '#') {
+      return 1 + getTitleLevel(titleLine.substring(1, titleLine.length));
+    }
+    else {
+      return 0;
+    }
+  }
 
+  function getTitleText(titleLine) {
+    if (titleLine !== "" && titleLine.charAt(0) === '#') {
+      return getTitleText(titleLine.substring(1, titleLine.length));
+    }
+    else {
+      return titleLine;
+    }
+  }
 
   mdTocUtils.getTitles = function(mdRawText){
-    return [
-      {level: 1, text: "titleLevel1"},
-      {level: 2, text: "titleLevel2"}
-    ];
+    var titles = [];
+    var textLines = mdRawText.split('\n');
+    for (var i = 0; i < textLines.length; i++) {
+      if (textLines[i].charAt(0) === '#') {
+        titles.push({
+          level: getTitleLevel(textLines[i]),
+          text: getTitleText(textLines[i])
+        });
+      }
+    }
+    return titles;
   };
 
+  function getTitleId(titleText) {
+    if (titleText !== "" && titleText.charAt(0) === ' ') {
+      return getTitleId(titleText.substring(1, titleText.length));
+    } else {
+      return titleText.replace(/\s/g, '-');
+    }
+  }
 
+  function getTocLine(title) {
+    tocLine = "";
+    for (var i = 1; i < title.level; i++) {
+      tocLine += '\t';
+    }
+    return tocLine+'- ['+title.text+'](#'+getTitleId(title.text)+')\n';
+  }
 
   mdTocUtils.generateTocFromTitles = function(titlesTab){
-    return {
-      generatedToc: "Generated TOC"
-    };
+    var result = {generatedToc: "Toc\n"};
+    for (title of titlesTab) {
+      result.generatedToc += getTocLine(title);
+    }
+    return result;
   }
 
   mdTocUtils.generateToc = function(mdRawText){
--- test/spec/mdTocUtils.js
+++ test/spec/mdTocUtils.js
@@ -5,58 +5,52 @@ describe("MdTocUtils", function() {
 
   describe("Markdown Parsing", function() {
 
-    it('should correctly parse Markdown', inject(['MdTocUtils',
-      function(MdTocUtils) {
+    it('parse with various hashtag titles', inject(['MdTocUtils', function(MdTocUtils) {
+      expect(MdTocUtils.getTitleLevel).not.toBe(null);
 
-        expect(MdTocUtils.getTitles).not.toBe(null);
-
-        var getTitlesTests = [
-          {
-            param: "#titleLevel1 \n ##titleLevel2",
-            expectedRes:  [
-              {level: 1, text: "titleLevel1"},
-              {level: 2, text: "titleLevel2"}
-            ]
-          },
-          {
-            param:
-            "#titleLevel1 \n ##titleLevel2",
-            expectedRes:  [
-              {level: 1, text: "titleLevel1"},
-              {level: 2, text: "titleLevel2"}
-            ]
-          }
+      var testCase = {
+        param:
+          "# bla \n"+
+          "fdbzenff efuznzejfnz ef\n"+
+          "##### #bla dbzehd eudhezdf \n"+
+          "##fdbzenff efuznzejfnz ef\n"+
+          " # fdbzenff efuznzejfnz ef\n"+
+          "fdbzenff efuznzejfnz ef\n"+
+          "fdbzenff efuznzejfnz ef\n",
+        expectedRes: [
+          {level:1 , text: " bla "},
+          {level:5 , text: " #bla dbzehd eudhezdf "},
+          {level:2 , text: "fdbzenff efuznzejfnz ef"}
         ]
+      };
 
-        for (var i = 0; i < getTitlesTests.length; i++) {
-          expect(MdTocUtils.getTitles(getTitlesTests[i].param))
-          .toEqual(getTitlesTests[i].expectedRes);
-        }
-      }])
-    );
+      expect(MdTocUtils.getTitles(testCase.param)).toEqual(testCase.expectedRes);
+
+    }]));
   });
 
   describe("Markdown TOC Generating", function() {
 
-    it('should correctly generate Toc', inject(['MdTocUtils',
+    it('generate toc for various titles tab with space only', inject(['MdTocUtils',
       function(MdTocUtils) {
 
         expect(MdTocUtils.generateTocFromTitles).not.toBe(null);
 
-        var generateTocTests = [
-          {
-            param: [
-              {level: 1, text: "titleLevel1"},
-              {level: 2, text: "titleLevel2"}
-            ],
-            expectedRes: "Generated TOC"
-          }
-        ]
-
-        for (var i = 0; i < generateTocTests.length; i++) {
-          expect(MdTocUtils.generateTocFromTitles(generateTocTests[i].param).generatedToc)
-          .toEqual(generateTocTests[i].expectedRes);
-        }
+        var testCase = {
+          param: [
+            {level:1 , text: " bla "},
+            {level:5 , text: " bla dbzehd eudhezdf "},
+            {level:2 , text: "fdbzenff efuznzejfnz ef"}
+          ],
+          expectedRes:
+            "Toc\n"+
+            "- [ bla ](#bla-)\n"+
+            "\t\t\t\t- [ bla dbzehd eudhezdf ](#bla-dbzehd-eudhezdf-)\n"+
+            "\t- [fdbzenff efuznzejfnz ef](#fdbzenff-efuznzejfnz-ef)\n"
+        };
+        console.log(MdTocUtils.generateTocFromTitles(testCase.param).generatedToc);
+        console.log(testCase.expectedRes);
+        expect(MdTocUtils.generateTocFromTitles(testCase.param).generatedToc).toEqual(testCase.expectedRes);
       }])
     );
   });
